3fef9ded48a6bda2da56cfa231be3fcd22b5e4fb
Amaya0221 <155319702+Amaya0221@users.noreply.github.com>
2026-04-18 16:38:59 -0400

    Update 02_queries_michael.sql

--- queries/02_queries_michael.sql
+++ queries/02_queries_michael.sql
@@ -24,22 +24,30 @@ GROUP BY
 
 -- Q06: What is the distribution of customer review scores (1–5)?
 SELECT 
-    review_score, 
-    COUNT(*) AS TotalReviews,
-    REPLICATE(N'★', review_score) AS Stars
-FROM order_reviews
+    review_score,
+    COUNT(*) AS TotalReviews, 
+    REPLICATE(N'★', review_score) AS RatingLabel,
+    FORMAT(COUNT(*), '#,###') AS TotalReviews_Formatted
+FROM order_status
 GROUP BY review_score
 ORDER BY review_score DESC;
 
 -- Q07: How many customers have placed more than one order?
-SELECT COUNT(*) AS RepeatCustomerCount
-FROM (
-    SELECT c.customer_unique_id
-    FROM Customers c
-    JOIN orders o ON c.customer_id = o.customer_id
-    GROUP BY c.customer_unique_id
-    HAVING COUNT(o.order_id) > 1
-) AS RepeatCustomers;
+SELECT 
+    FORMAT(o.order_purchase_timestamp, 'yyyy-MM') AS OrderMonth,
+    COUNT(DISTINCT c.customer_unique_id) AS RepeatCustomerCount
+FROM Customers c
+JOIN olist_orders_dataset o ON c.customer_id = o.customer_id
+WHERE c.customer_unique_id IN (
+
+    SELECT c2.customer_unique_id
+    FROM Customers c2
+    JOIN olist_orders_dataset o2 ON c2.customer_id = o2.customer_id
+    GROUP BY c2.customer_unique_id
+    HAVING COUNT(o2.order_id) > 1
+)
+GROUP BY FORMAT(o.order_purchase_timestamp, 'yyyy-MM')
+ORDER BY OrderMonth;
 
 -- Q08: What is the breakdown of all orders by their current status?
 SELECT 
